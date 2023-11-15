e8c48a3d80a043a6fc0f3af9e8a90e0f2ccb2674
Julie Dessart <julie.destras@gmail.com>
2023-11-15 13:53:04 +0100

    Liaison BDD, création des modèles + vues espace admin

--- mercadona/app/admin.py
+++ mercadona/app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import Category, Product, Discount
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('product','discount_percentage','discounted_price', 'start_date', 'end_date', 'active')
+    readonly_fields = ['discounted_price', 'active'] 
+    
+class DiscountInline(admin.TabularInline):
+    model = Discount  
+    fields = ['discount_percentage','start_date', 'end_date','discounted_price']
+    readonly_fields = ['discounted_price',] 
+    extra = 1
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'initial_price', 'display_categories', 'thumbnail')
+    list_filter = ('categories', 'name')
+    search_fields = ('name', 'description')
+    change_form_template = 'admin/discount_form.html'
+    fields = ['name', 'categories', 'description', 'thumbnail', 'thumbnail_title','initial_price']
+    inlines = [DiscountInline]  
+
+    def display_categories(self, obj):
+            return",".join([category.name for category in obj.categories.all()])
--- mercadona/mercadona/settings.py
+++ mercadona/mercadona/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q518&1h4a6s!k-jj*1&^(f3y!fi)0c8&74i0(7e2%me36ylqx5'
-
+SECRET_KEY = os.environ.get("SECRET_KEY"),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'mercadona.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'app','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +78,14 @@ WSGI_APPLICATION = 'mercadona.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
